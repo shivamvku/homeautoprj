8e5f0387ef8ea359e8e9e865434b5e6796d497c7
shivamvku <shivam.vku@gmail.com>
2023-10-07 11:20:26 +0530

    inital commit for app

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from typing import Union
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0", port=5000, log_level="debug", reload="true")
